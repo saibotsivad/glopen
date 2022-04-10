53128bddb2557fe1346421cb3b39327c8fff9563
Tobias Davis <tobias@davistobias.com>
2022-04-10 16:16:54 -0500

    feature(gen): copy license file to each dir

---
+++ scripts/copy-license-to-folders.js
@@ -0,0 +1,20 @@
+import { join } from 'node:path'
+import { readdir, readFile, writeFile } from 'node:fs/promises'
+
+const LICENSE = await readFile('./LICENSE.md', 'utf8')
+
+const getDirs = name => readdir(name, { withFileTypes: true })
+	.then(dirEnts => dirEnts
+		.filter(d => d.isDirectory())
+		.map(d => join(name, d.name))
+	)
+
+const dirs = await Promise.all([
+	getDirs('controllers'),
+	getDirs('definitions'),
+	getDirs('packages'),
+]).then(all => all.flat())
+
+for (const dir of dirs) {
+	writeFile(join(dir, 'LICENSE.md'), LICENSE, 'utf8')
+}
